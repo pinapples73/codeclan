940bcb4a583d2e79b7f44623f3de1393c3cb7cc9
Gordon Mitchell <gordymitch@gmail.com>
2020-02-12 22:04:25 +0000

    finished all extensions

--- week06/day03/homework/hw_advanced_enumeration/anagram_finder/anagram_finder.js
+++ week06/day03/homework/hw_advanced_enumeration/anagram_finder/anagram_finder.js
@@ -1,9 +1,40 @@
 const AnagramFinder = function (word) {
+  this.originalWord = word.toLowerCase();
+  this.word = word.toLowerCase();
+  this.word = this.word.split('');
+  this.word = this.word.sort();
+  this.word = this.word.join('');
+  this.word = this.word.toLowerCase();
+
 
 }
+
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  const returnArray = [];
+  const result = otherWords.filter((wordTwo) => {
+    if (wordTwo !== this.originalWord) {
+      let splitWord;
+      splitWord = wordTwo.toLowerCase();
+      splitWord = splitWord.split('');
+      splitWord = splitWord.sort();
+      splitWord = splitWord.join('');
 
-}
+      if (this.word === splitWord) {
+        returnArray.push(wordTwo);
+      };
+    };
+  });
+  return returnArray;
+};
 
 module.exports = AnagramFinder;
+
+
+// (anagramFinder.findAnagrams(['cat', 'dog']), ['cat']);
+//
+// return this.journeys.filter((journey) => {
+//     if (journey.distance > minDistance) {
+//       return journey;
+//     };
+//   });
