414dcf3d7adb36291f2483a3481038638a9b4ad4
Gordon Mitchell <gordymitch@gmail.com>
2020-02-12 18:13:20 +0000

    extensions uppercase and pangram

--- week06/day03/homework/hw_advanced_enumeration/pangram_finder/pangram_finder.js
+++ week06/day03/homework/hw_advanced_enumeration/pangram_finder/pangram_finder.js
@@ -1,9 +1,22 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
+  const lowerCasePhrase = this.phrase.toLowerCase();
 
-}
+  const result = this.alphabet.every((letter) => {
+    return lowerCasePhrase.includes(letter);
+  });
+  return result;
+};
+
+// Cinema.prototype.anyFilmsByYear = function (year) {
+//   let result = this.films.some((film) => {
+//     return film.year === year;
+//   });
+//   return result;
+// };
 
 module.exports = PangramFinder;
